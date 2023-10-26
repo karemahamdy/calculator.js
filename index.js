f0faff00d19a52f0c5962c2ff70986d8020c5649
Karema Hamdy <131587176+karemahamdy@users.noreply.github.com>
2023-10-26 23:00:39 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let click = document.querySelector("input");
+
+function appendNumber(number) {
+    if (number == ""){
+        document.querySelector("input").value = "";
+    }
+    else{
+    document.querySelector("input").value += number;
+    }
+  }
+
+  function appendOperator(operator) {
+    document.querySelector("input").value += operator;
+  }
+  function deleteNumber() {
+    let currentResult = document.querySelector("input").value;
+    document.querySelector("input").value = currentResult.slice(0, -1);
+  }
